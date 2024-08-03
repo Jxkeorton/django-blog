e283ef72084699aab4bb1419d901a43f11f61afb
Jxkeorton <jakeorton99@gmail.com>
2024-08-03 11:02:58 +0100

    Create basic files and file paths

--- codestar/urls.py
+++ codestar/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('blog/', include('blog.urls'), name="blog-urls"),
+    path('', include('blog.urls'), name="blog-urls"),
 ]
