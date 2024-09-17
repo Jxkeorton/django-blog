82f7d9129dfdb310128b1846dd93da209f6024bb
Jxkeorton <jakeorton99@gmail.com>
2024-09-17 17:27:28 +0100

    new commit

--- env.py
+++ env.py
@@ -3,3 +3,7 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://udlilkickob:BZcNuXp5Sifa@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/turf_gong_prior_569010")
 os.environ.setdefault("SECRET_KEY", "codestar")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://879688624236174:_EIHUOkwSqNQV0JhDNJ6WIkqAG0@dz02qubd3"
+)
